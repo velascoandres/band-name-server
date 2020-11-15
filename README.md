4a0a91e1c2c788e0e4173d98a5a1089ebf70a222
andres velasco <velasco.andrs@gmail.com>
2020-11-14 21:45:35 -0500

    init server files

---
+++ README.md
@@ -0,0 +1,2 @@
+## BandNames backend
+
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+app.listen(
+    3000,
+    err => {
+        if (err) throw new Error(err);
+        console.log('Servidor corriendo en el puerto 3000');
+    }   
+);
+
+
