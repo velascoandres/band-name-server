a8e88370df034b4fa0a318f582268e04282761bc
andres velasco <velasco.andrs@gmail.com>
2020-11-14 23:13:13 -0500

    configuracion de sockets

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express from 'express'
 import path from 'path';
 import dotenv from 'dotenv';
+import http from 'http';
+import { setupSockets } from './sockets';
+
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+
+// sockets
+const server = http.createServer(app);
+setupSockets(server, port);
 
 // path publico
 
@@ -14,15 +21,6 @@ const pathPublico = path.resolve('public');
 
 
 app.use(express.static(pathPublico));
-
-
-app.listen(
-    port,
-    () => {
-        console.log(pathPublico);
-        console.log(`Servidor corriendo en el puerto ${port}`);
-    }
-);
 
 
 
---
+++ src/sockets.ts
@@ -0,0 +1,30 @@
+// Mensajes de sockets
+export function setupSockets(server: any, port: number | string) {
+    const io = require('socket.io')(server);
+    io.on('connection',
+        (client: any) => {
+
+            console.log('Cliente conectado');
+
+            client.on(
+                'disconnect',
+                () => { /* … */ },
+            );
+            client.on(
+                'mensaje',
+                (data: any) => {
+                    console.log('mensaje: ', data);
+                    io.emit('mensaje', { admin: 'Nuevo mensaje' });
+                },
+            );
+        },
+    );
+
+    server.listen(
+        port,
+        () => {
+            console.log(`Servidor corriendo en el puerto ${port}`);
+        }
+    );
+
+}
